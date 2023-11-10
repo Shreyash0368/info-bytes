af24b838cba86cbe447d01c73be638058b5dc7aa
Shreyash0368 <shreyashgpt0503@gmail.com>
2023-11-10 11:31:16 +0530

    added infinite scroll, top-loader and .env.var

--- src/App.js
+++ src/App.js
@@ -11,19 +11,20 @@ import Navbar from './components/Navbar';
 import NewsCont from './components/NewsCont';
 
 export class App extends Component {
+  apiKey = process.env.REACT_APP_NEWS_API
   render() {
     return (
       <Router>
         <div>
           <Navbar />
           <Routes>
-            <Route path='/'  element={<NewsCont key={'general'} pageSize={8} category={'general'} country={'in'} />}></Route>
-            <Route path='/health'  element={<NewsCont key={'health'} pageSize={8} category={'health'} country={'in'} />}></Route>
-            <Route path='/science'  element={<NewsCont key={'science'} pageSize={8} category={'science'} country={'in'} />}></Route>
-            <Route path='/technology' element={<NewsCont key={'technology'} pageSize={8} category={'technology'} country={'in'} />}></Route>
-            <Route path='/entertainment' element={<NewsCont key={'entertainment'} pageSize={8} category={'entertainment'} country={'in'} />}></Route>
-            <Route path='/sports' element={<NewsCont key={'sports'} pageSize={8} category={'sports'} country={'in'} />}></Route>
-            <Route path='/business' element={<NewsCont key={'business'} pageSize={8} category={'business'} country={'in'} />}></Route>
+            <Route path='/'  element={<NewsCont key={'general'} pageSize={8} category={'general'} country={'in'} apiKey={process.env.REACT_APP_NEWS_API} />}></Route>
+            <Route path='/health'  element={<NewsCont key={'health'} pageSize={8} category={'health'} country={'in'} apiKey={process.env.REACT_APP_NEWS_API} />}></Route>
+            <Route path='/science'  element={<NewsCont key={'science'} pageSize={8} category={'science'} country={'in'} apiKey={process.env.REACT_APP_NEWS_API} />}></Route>
+            <Route path='/technology' element={<NewsCont key={'technology'} pageSize={8} category={'technology'} country={'in'} apiKey={process.env.REACT_APP_NEWS_API} />}></Route>
+            <Route path='/entertainment' element={<NewsCont key={'entertainment'} pageSize={8} category={'entertainment'} country={'in'} apiKey={process.env.REACT_APP_NEWS_API} />}></Route>
+            <Route path='/sports' element={<NewsCont key={'sports'} pageSize={8} category={'sports'} country={'in'} apiKey={process.env.REACT_APP_NEWS_API} />}></Route>
+            <Route path='/business' element={<NewsCont key={'business'} pageSize={8} category={'business'} country={'in'} apiKey={process.env.REACT_APP_NEWS_API} />}></Route>
           </Routes>
 
 
--- src/components/NewsCont.js
+++ src/components/NewsCont.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import NewsCard from './NewsCard'
 import PlaceholderImg from '../../src/images/placeholder-news.jpg'
 import Spinner from './Spinner';
+import InfiniteScroll from "react-infinite-scroll-component";
+import LoadingBar from 'react-top-loading-bar'
 
 
 export class NewsCont extends Component {
@@ -15,7 +17,8 @@ export class NewsCont extends Component {
     static propTypes = {
         country: PropTypes.string,
         pageSize: PropTypes.number,
-        category: PropTypes.string
+        category: PropTypes.string,
+        apiKey: PropTypes.string
     }
 
 
@@ -25,60 +28,75 @@ export class NewsCont extends Component {
             articles: [],
             loading: false,
             page: 1,
-            limit: 1
+            results: 0,
+            progress: 0
         }
     }
     async componentDidMount() {
-       
+
         await this.fetchNews();
     }
 
     async fetchNews() {
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&language=en&apiKey=967158990b304bf4a3083f87c3c27455&pageSize=${this.props.pageSize}&page=${this.state.page}`;
+        this.setState({progress : 20})
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&language=en&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`;
         this.setState({
             loading: true
         })
         const data = await fetch(url);
+        this.setState({progress : 40})
         const parsedData = await data.json();
+        this.setState({progress : 60})
         this.setState({
             articles: parsedData.articles,
-            limit: Math.ceil(parsedData.totalResults / 20),
-            loading: false
+            loading: false,
+            results: parsedData.totalResults
         });
+        this.setState({progress : 100})
+        // this.setState({progress : 0})
     }
 
-    nextPage = () => {
+    fetchMoreData = async() => {
+        console.log(this.props.apiKey);
+        this.setState({progress : 20})
         this.state.page++;
-        console.log(this.state.page);
-        this.fetchNews();
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&language=en&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`;
+        
+        const data = await fetch(url);
+        this.setState({progress : 40})
+        const parsedData = await data.json();
+        this.setState({progress : 60})
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            loading: false,
+        });
+        this.setState({progress : 100})
     }
-    prevPage = () => {
-        this.state.page--;
-        console.log(this.state.page);        
-        this.fetchNews();
-    }
+
+    
     render() {
 
         return (
-
-            <div className='container my-6 '>
+            <>
+                <LoadingBar progress={this.state.progress} color='#3EB489'/>           
                 <h2 className='my-4 text-center'>Info-Bytes - Byte Sized News</h2>
                 {this.state.loading && <Spinner />}
-                {!this.state.loading && <div className="row row-cols-1 row-cols-sm-4 my-4">
-                    {this.state.articles.map((el) => {
-                        return <div className="col" key={el.url}>
-                            <NewsCard title={el.title} description={el.description} imageURL={el.urlToImage ? el.urlToImage : PlaceholderImg} newsURL={el.url} author={el.author} date={el.publishedAt} publisher={el.source.name} />
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.results}
+                    loader={<Spinner />}>
+                    <div className='container my-6 '>
+                        <div className="row row-cols-1 row-cols-sm-4 my-4">
+                            {this.state.articles.map((el) => {
+                                return <div className="col" key={el.url}>
+                                    <NewsCard title={el.title} description={el.description} imageURL={el.urlToImage ? el.urlToImage : PlaceholderImg} newsURL={el.url} author={el.author} date={el.publishedAt} publisher={el.source.name} />
+                                </div>
+                            })}
                         </div>
-                    })}
-                    <div className="d-flex justify-content-between w-100">
-                        <button type="button" disabled={(this.state.page <= 1)} className="btn btn-dark" onClick={this.prevPage}>Previous</button>
-                        <button type="button" disabled={(this.state.page >= this.state.limit)} className="btn btn-dark" onClick={this.nextPage}>Next</button>
                     </div>
-                </div>}
-
-
-
-            </div>
+                </InfiniteScroll>
+            </>
         )
     }
 }
